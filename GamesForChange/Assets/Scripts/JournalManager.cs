d2f48b1c0b7091c00dcb5f7a4fb9d3962cfe6657
ShaelinB <shlb53@icloud.com>
2025-02-25 21:43:17 -0500

    made journal open/close/flip pages

---
+++ GamesForChange/Assets/Scripts/JournalManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class JournalManager : MonoBehaviour
+{
+    public bool isBookOpen = false;
+    public int startingPage;
+    public int currentPage;
+    public GameObject canvas;
+    public List<GameObject> pages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < pages.Count; i++)
+        {
+            pages[i].SetActive(false);
+        }
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && !isBookOpen)
+        {
+            isBookOpen = true;
+            canvas.SetActive(true);
+            currentPage = startingPage;
+            pages[startingPage].SetActive(true);
+            pages[startingPage + 1].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.J) && isBookOpen) 
+        {
+            isBookOpen = false;
+            canvas.SetActive(false);
+            startingPage = currentPage;
+        }
+        if(isBookOpen)
+        {
+            FlipPage();
+        }
+    }
+
+    void FlipPage()
+    {
+        Debug.Log("in flip page");
+        if (Input.GetMouseButtonDown(1) && currentPage != pages.Count - 2)
+        {
+            Debug.Log("flip forward");
+            pages[currentPage].gameObject.SetActive(false);
+            pages[currentPage+1].gameObject.SetActive(false);
+
+            currentPage += 2;
+
+            pages[currentPage].gameObject.SetActive(true);
+            pages[currentPage + 1].gameObject.SetActive(true);
+
+        }
+        else if (Input.GetMouseButtonDown(0) && currentPage != 0)
+        {
+            Debug.Log("flip backward");
+            pages[currentPage].gameObject.SetActive(false);
+            pages[currentPage + 1].gameObject.SetActive(false);
+
+            currentPage -= 2;
+
+            pages[currentPage].gameObject.SetActive(true);
+            pages[currentPage + 1].gameObject.SetActive(true);
+        }
+    }
+
+
+}
