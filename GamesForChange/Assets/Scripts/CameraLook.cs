c93c0752afe4a00c65639d429a284fa216dc70fa
WillImb <143808397+WillImb@users.noreply.github.com>
2025-03-13 13:51:58 -0400

    character controller

---
+++ GamesForChange/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Transform lockPosition;
+    float camX;
+    float camY;
+    public float sens;
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+        camX -= Input.GetAxisRaw("Mouse Y") * sens;
+        camX = Mathf.Clamp(camX, -90, 90);
+
+        camY += Input.GetAxisRaw("Mouse X") * sens;
+
+        transform.rotation = Quaternion.Euler(new Vector3(camX, camY, 0));
+
+
+        transform.position = lockPosition.position;
+    }
+}
---
+++ GamesForChange/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera cam;
+    public float speed;
+
+ 
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, cam.transform.eulerAngles.y, transform.rotation.z);
+
+        
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        rb.velocity = move * speed *Time.fixedDeltaTime;
+    }
+}
